824605b946f7c7425f248ee0c24e6c20e887deaa
Gaya Kessler <gaya@theclevernode.com>
2019-03-15 11:10:47 +0100

    handle tracking and save tracked variants

--- src/ab-test-tracking.php
+++ src/ab-test-tracking.php
@@ -25,19 +25,32 @@ class ABTestTracking {
 
         $variants = $this->abTestManager->getEnabledVariantsByGoal($postId);
 
+        if (!isset($cookieData['tracked'])) {
+            $cookieData['tracked'] = [];
+        }
+
         $tracked = [];
 
         foreach ($variants as $variant) {
             if (!$variant['isEnabled']) continue;
 
             if (
-                isset($cookieData[$variant['testId']]) 
+                // not already tracked
+                !in_array($variant['variantId'], $cookieData['tracked'])
+                // if in this tests participants
+                && isset($cookieData[$variant['testId']])
+                // actually in this variant
                 && $cookieData[$variant['testId']] === $variant['variantId']
             ) {
                 array_push($tracked, $variant['variantId']);
                 $this->abTestManager->addTracking($variant['variantId'], 'C');
             }
         }
+
+        // add tracked variants to cookie
+        $cookieData['tracked'] = array_merge([], $cookieData['tracked'], $tracked);
+
+        setcookie('ab-testing-for-wp', json_encode($cookieData), time() + (60*60*24*30), '/');
 
         return rest_ensure_response($tracked);
     }
