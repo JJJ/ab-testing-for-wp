e155de8ea9b639b57fcb35f5591ac61f96b802f2
Gaya Kessler <gaya@theclevernode.com>
2019-03-15 11:27:21 +0100

    handle cookies in php

--- src/scripts/frontend/handleTestRender.js
+++ src/scripts/frontend/handleTestRender.js
@@ -1,16 +1,9 @@
 // @flow
 
-import Cookies from 'js-cookie';
-
 const { apiFetch } = window.wp;
 
 function handleTestRender() {
-  const cookieKey = 'ab-testing-for-wp';
-  const cookieData = JSON.parse(Cookies.get(cookieKey) || '{}');
-
   const testsOnPage = document.getElementsByClassName('ABTestWrapper');
-
-  const newData = {};
 
   for (let i = 0; i < testsOnPage.length; i += 1) {
     const test = testsOnPage[i];
@@ -23,10 +16,6 @@ function handleTestRender() {
         if (variant.html) {
           test.innerHTML = variant.html;
         }
-
-        newData[testId] = variant.id;
-
-        Cookies.set(cookieKey, Object.assign({}, cookieData, newData), { expires: 30 });
       });
   }
 }
