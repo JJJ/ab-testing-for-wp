b594405327c8a64793fef9621aaea33c94e6274d
Gaya Kessler <gaya.kessler@gmail.com>
2019-03-25 19:01:49 +0100

    add icon to menu item

--- src/register-frontend-admin-bar.php
+++ src/register-frontend-admin-bar.php
@@ -8,7 +8,14 @@ class RegisterFrontendAdminBar {
     public function __construct($fileRoot) {
         $this->fileRoot = $fileRoot;
 
-        add_action('admin_bar_menu', [$this, 'addAdminBarMenu']);
+        $this->loadStyles();
+
+        add_action('admin_bar_menu', [$this, 'addAdminBarMenu'], 90);
+    }
+
+    public function loadStyles() {
+        wp_register_style('ab_testing_for_wp_frontend_style', plugins_url('/src/css/frontend.css', $this->fileRoot), []);
+        wp_enqueue_style('ab_testing_for_wp_frontend_style');
     }
 
     public function addAdminBarMenu($wp_admin_bar) {
