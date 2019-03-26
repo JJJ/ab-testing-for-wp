7fd8a89b9c46ecbc8f8535751109f83d9d9ce997
Gaya Kessler <gaya@theclevernode.com>
2019-03-26 13:28:32 +0100

    ability to focus to tests

--- src/scripts/admin-bar.js
+++ src/scripts/admin-bar.js
@@ -2,61 +2,8 @@
 
 import scrollIntoView from 'scroll-into-view';
 
-let lastHighlighted;
-
-function offsetFromRects(rect) {
-  const scrollLeft = window.pageXOffset
-    || (document.documentElement || { scrollLeft: 0 }).scrollLeft;
-  const scrollTop = window.pageYOffset
-    || (document.documentElement || { scrollTop: 0 }).scrollTop;
-  return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
-}
-
-function highLightElement(testId: string, node: HTMLElement) {
-  if (lastHighlighted === testId) return;
-
-  lastHighlighted = testId;
-
-  const highLightId = `ab-testing-for-wp__highlight__${testId}`;
-
-  if (document.getElementById(highLightId)) return;
-
-  document.querySelectorAll('.ab-testing-for-wp__highlight').forEach((item) => {
-    if (!document.body) return;
-    document.body.removeChild(item);
-  });
-
-  const boundingRects = node.getBoundingClientRect();
-  const offset = offsetFromRects(boundingRects);
-  const highlight = document.createElement('div');
-  const padding = 15;
-
-  highlight.className = 'ab-testing-for-wp__highlight';
-  highlight.setAttribute('id', highLightId);
-
-  highlight.style.position = 'absolute';
-  highlight.style.width = `${boundingRects.width + (padding * 2)}px`;
-  highlight.style.height = `${boundingRects.height + (padding * 2)}px`;
-  highlight.style.top = `${offset.top - padding}px`;
-  highlight.style.left = `${offset.left - padding}px`;
-
-  if (!document.body) return;
-
-  document.body.appendChild(highlight);
-
-  setTimeout(() => {
-    if (!document.body || !document.body.contains(highlight)) return;
-    document.body.removeChild(highlight);
-  }, 5000);
-}
-
-function findTestById(id: string) {
-  return document.querySelector(`.ABTestWrapper[data-test=${id}]`);
-}
-
-function findId(node: HTMLElement) {
-  return node.id.replace('wp-admin-bar-ab-testing-for-wp_', '');
-}
+import { findId, findTestById } from './helpers/test-dom';
+import highLightElement from './helpers/highlight';
 
 function handleTest() {
   const testId = findId(this);
@@ -84,5 +31,40 @@ function bindAdminBarEvents() {
   });
 }
 
+function setTestStatus() {
+  if (!window.ABTestingForWP_AdminBar) return;
 
-document.addEventListener('DOMContentLoaded', bindAdminBarEvents);
+  const data = window.ABTestingForWP_AdminBar;
+  const prefix = 'wp-admin-bar-ab-testing-for-wp_';
+
+  // remove current state
+  document
+    .querySelectorAll('.ab-testing-for-wp__test')
+    .forEach(node => node.classList.remove('ab-testing-for-wp__enabled'));
+
+  document
+    .querySelectorAll('.ab-testing-for-wp__variant')
+    .forEach(node => node.classList.remove('ab-testing-for-wp__selected'));
+
+  data.testsData.forEach((test) => {
+    const element = document.getElementById(`${prefix}${test.id}`);
+
+    if (!element) return;
+
+    if (test.isEnabled) element.classList.add('ab-testing-for-wp__enabled');
+
+    test.variants.forEach((variant) => {
+      const variantElement = document.getElementById(`${prefix}${variant.id}`);
+
+      if (!variantElement) return;
+      if (data.cookieData[test.id] === variant.id) variantElement.classList.add('ab-testing-for-wp__selected');
+    });
+  });
+}
+
+function initAdminBar() {
+  setTestStatus();
+  bindAdminBarEvents();
+}
+
+document.addEventListener('DOMContentLoaded', initAdminBar);
---
+++ src/scripts/helpers/highlight.js
@@ -0,0 +1,49 @@
+// @flow
+
+let lastHighlighted;
+
+function offsetFromRects(rect) {
+  const scrollLeft = window.pageXOffset
+    || (document.documentElement || { scrollLeft: 0 }).scrollLeft;
+  const scrollTop = window.pageYOffset
+    || (document.documentElement || { scrollTop: 0 }).scrollTop;
+  return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+}
+
+export default function highLightElement(testId: string, node: HTMLElement) {
+  if (lastHighlighted === testId) return;
+
+  lastHighlighted = testId;
+
+  const highLightId = `ab-testing-for-wp__highlight__${testId}`;
+
+  if (document.getElementById(highLightId)) return;
+
+  document.querySelectorAll('.ab-testing-for-wp__highlight').forEach((item) => {
+    if (!document.body) return;
+    document.body.removeChild(item);
+  });
+
+  const boundingRects = node.getBoundingClientRect();
+  const offset = offsetFromRects(boundingRects);
+  const highlight = document.createElement('div');
+  const padding = 15;
+
+  highlight.className = 'ab-testing-for-wp__highlight';
+  highlight.setAttribute('id', highLightId);
+
+  highlight.style.position = 'absolute';
+  highlight.style.width = `${boundingRects.width + (padding * 2)}px`;
+  highlight.style.height = `${boundingRects.height + (padding * 2)}px`;
+  highlight.style.top = `${offset.top - padding}px`;
+  highlight.style.left = `${offset.left - padding}px`;
+
+  if (!document.body) return;
+
+  document.body.appendChild(highlight);
+
+  setTimeout(() => {
+    if (!document.body || !document.body.contains(highlight)) return;
+    document.body.removeChild(highlight);
+  }, 5000);
+}
