f86b8c49110c3f3944cbf0e119be1d488a744f1b
Gaya Kessler <gaya.kessler@gmail.com>
2019-03-28 11:16:46 +0100

    update autoload mapping

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,12 +8,13 @@ class ComposerStaticInit3ca069ed2eeb7caff92dca41e2d25775
 {
     public static $classMap = array (
         'ABTestingForWP\\ABTestContentParser' => __DIR__ . '/../..' . '/src/ab-test-content-parser.php',
-        'ABTestingForWP\\ABTestManager' => __DIR__ . '/../..' . '/src/ab-test-manager.php',
+        'ABTestingForWP\\ABTestManager' => __DIR__ . '/../..' . '/src/data/ab-test-manager.php',
         'ABTestingForWP\\ABTestStats' => __DIR__ . '/../..' . '/src/ab-test-stats.php',
         'ABTestingForWP\\ABTestTracking' => __DIR__ . '/../..' . '/src/ab-test-tracking.php',
         'ABTestingForWP\\AdminPage' => __DIR__ . '/../..' . '/src/admin-page.php',
         'ABTestingForWP\\BlockRenderer' => __DIR__ . '/../..' . '/src/block-renderer.php',
         'ABTestingForWP\\Installer' => __DIR__ . '/../..' . '/src/installer.php',
+        'ABTestingForWP\\PostsActions' => __DIR__ . '/../..' . '/src/actions/posts.php',
         'ABTestingForWP\\RegisterFrontendAdminBar' => __DIR__ . '/../..' . '/src/register-frontend-admin-bar.php',
         'ABTestingForWP\\RegisterGutenbergBlocks' => __DIR__ . '/../..' . '/src/register-gutenberg-blocks.php',
         'ABTestingForWP\\RegisterREST' => __DIR__ . '/../..' . '/src/register-rest.php',
