7fa8580249537e06c1f14c101fa1cce083140e29
Gaya Kessler <gaya@theclevernode.com>
2019-04-01 09:59:12 +0200

    select post type from rest api

--- src/js/components/PostSelector/PostSelector.js
+++ src/js/components/PostSelector/PostSelector.js
@@ -19,43 +19,115 @@ type PostSelectorProps = {
 
 type PostSelectorState = {
   loading: boolean;
-  pages: any[];
+  selectedType: string;
+  posts: {
+    title: {
+      rendered: string;
+    };
+    id: number;
+  }[];
+  types: {
+    [key: string]: {
+      name: string;
+      slug: string;
+    };
+  };
 };
 
 class PostSelector extends Component<PostSelectorProps, PostSelectorState> {
   state = {
     loading: true,
-    pages: [],
+    posts: [],
+    types: {},
+    selectedType: '',
   };
 
-  componentDidMount() {
-    const pageId = data.select('core/editor').getCurrentPostId();
+  forbiddenTypes = [];
 
-    apiFetch({ path: `/wp/v2/pages?per_page=100&exclude=${pageId}` })
-      .then((pages) => {
+  componentDidMount() {
+    const { value } = this.props;
+
+    const postId = data.select('core/editor').getCurrentPostId();
+
+    let resolvePostType = Promise.resolve('');
+
+    if (value) {
+      resolvePostType = apiFetch({ path: `/ab-testing-for-wp/v1/get-post-type?post_id=${postId}` });
+    }
+
+    Promise.all([
+      resolvePostType,
+      apiFetch({ path: '/wp/v2/types' }),
+    ]).then(([postType, types]) => {
+      const filteredTypes = { ...types };
+      this.forbiddenTypes.forEach(type => delete filteredTypes[type]);
+
+      // auto select first result
+      const selectedType = postType === ''
+        ? filteredTypes[Object.keys(filteredTypes)[0]].slug
+        : postType.toString();
+
+      this.setState({
+        types: filteredTypes,
+        selectedType,
+      });
+
+      this.getPostsOfType(selectedType);
+    });
+  }
+
+  getPostsOfType(type: string) {
+    const postId = data.select('core/editor').getCurrentPostId();
+
+    apiFetch({ path: `/wp/v2/posts?type=${type}&per_page=100&exclude=${postId}` })
+      .then((posts) => {
         this.setState({
-          pages,
+          posts,
           loading: false,
         });
       });
   }
 
+  changePostType = (selectedType: string) => {
+    this.setState({ selectedType });
+    this.getPostsOfType(selectedType);
+  };
+
   render() {
-    const { loading, pages } = this.state;
+    const {
+      loading,
+      posts,
+      types,
+      selectedType,
+    } = this.state;
     const { onChange, value } = this.props;
 
     return (
       <PanelBody title={__('Testing Goal')}>
         {!loading && (
-          <SelectControl
-            value={value || 0}
-            options={[
-              { label: __('No goal selected'), value: 0 },
-              ...pages.map(page => ({ label: page.title.rendered, value: page.id })),
-            ]}
-            onChange={newValue => onChange(parseInt(newValue, 10))}
-            help={__('Goal page for this test. If the visitor lands on this page it will add a point to the tested variant.')}
-          />
+          <div>
+            <SelectControl
+              label={__('Type')}
+              value={selectedType}
+              options={[
+                ...Object.keys(types).map(type => ({
+                  label: types[type].name,
+                  value: types[type].slug,
+                })),
+              ]}
+              onChange={this.changePostType}
+            />
+            <SelectControl
+              label={__('Post')}
+              value={value || 0}
+              options={[
+                { label: __('No goal selected'), value: 0 },
+                ...posts.map(post => ({ label: post.title.rendered, value: post.id })),
+              ]}
+              onChange={newValue => onChange(parseInt(newValue, 10))}
+              help={__('Goal page for this test. If the visitor lands on this page it will add a point to the tested variant.')}
+            />
+          </div>
         )}
       </PanelBody>
     );
