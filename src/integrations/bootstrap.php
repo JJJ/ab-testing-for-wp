67917665c8be4ef3e4dc2a80d479cc54c0e16227
Gaya Kessler <gaya@theclevernode.com>
2019-04-03 16:42:41 +0200

    use init to catch

--- src/integrations/bootstrap.php
+++ src/integrations/bootstrap.php
@@ -5,8 +5,7 @@ namespace ABTestingForWP;
 class BootStrapIntegrations {
 
     public function __construct() {
-        add_action('admin_init', [$this, 'loadIntegrations']);
-        add_action('ab-testing-for-wp_rest_init', [$this, 'loadIntegrations']);
+        add_action('init', [$this, 'loadIntegrations']);
     }
 
     public function loadIntegrations() {
