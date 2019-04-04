3a11c0ac0be1c825582735df6a649f6cb9e2d684
Gaya Kessler <gaya.kessler@gmail.com>
2019-04-04 19:54:08 +0200

    calculate significance and show advise

---
+++ src/js/components/TestResults/Calc.js
@@ -0,0 +1,49 @@
+// @flow
+
+import type { TestResult } from './TestResults';
+
+function convertRatio(p: number, c: number) {
+  return p === 0 ? 0 : c / p;
+}
+
+function getCRSE(p: number, c: number) {
+  const cr = convertRatio(p, c);
+  return [
+    cr,
+    (cr * (1 - cr) / p) ** 0.5,
+  ];
+}
+
+function findWinner(results: TestResult[]) {
+  return results
+    .find((result) => {
+      const cr = convertRatio(result.participants, result.conversions);
+      return results.every(item => convertRatio(item.participants, item.conversions) <= cr);
+    });
+}
+
+function calc(control: string, results: TestResult[]) {
+  const winner = findWinner(results);
+
+  if (!winner) return null;
+
+  const controlVariant = winner.id === control
+    ? findWinner(results.filter(result => result.id !== control))
+    : results.find(result => result.id === control);
+
+  if (!controlVariant) throw new Error('Control variant of test cannot be found.');
+
+  const [crc, sea] = getCRSE(controlVariant.participants, controlVariant.conversions);
+  const [crr, ser] = getCRSE(winner.participants, winner.conversions);
+
+  const sed = ((sea ** 2) + (ser ** 2)) ** 0.5;
+  const zScore = (crr - crc) / sed;
+  const minScore = 1.96;
+
+  return {
+    winner,
+    confident: zScore > minScore,
+  };
+}
+
+export default calc;
