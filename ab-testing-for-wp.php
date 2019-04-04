620e05158bfd5017293878fa6a596813edd627e2
Gaya Kessler <gaya.kessler@gmail.com>
2019-04-04 12:11:32 +0200

    remove error reporting

--- ab-testing-for-wp.php
+++ ab-testing-for-wp.php
@@ -27,10 +27,6 @@
 
 namespace ABTestingForWP;
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 if (!defined('ABSPATH')) {
 	header('Status: 403 Forbidden');
 	header('HTTP/1.1 403 Forbidden');
