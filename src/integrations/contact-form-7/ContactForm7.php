f65a7ad478e4ac8209b756d1fa2d019943ec91ea
Gaya Kessler <gaya@theclevernode.com>
2019-04-04 09:06:24 +0200

    integrate contact form 7

--- src/integrations/contact-form-7/ContactForm7.php
+++ src/integrations/contact-form-7/ContactForm7.php
@@ -2,26 +2,26 @@
 
 namespace ABTestingForWP;
 
-class HTMLForms extends Integration {
+class ContactForm7 extends Integration {
 
-    protected $pluginSlug = 'html-forms/html-forms.php';
+    protected $pluginSlug = 'contact-form-7/wp-contact-form-7.php';
 
     protected function extraPluginCheck() {
         // check if code is really loaded
-        return function_exists('HTML_Forms\\_bootstrap');
+        return defined('WPCF7_VERSION');
     }
 
     protected function loadIntegration() {
         add_filter('ab-testing-for-wp_goal-types', [$this, 'addGoalType']);
-        add_action('hf_form_success', [$this, 'catchFormSubmits']);
+        add_action('wpcf7_submit', [$this, 'catchFormSubmits']);
     }
     
     public function addGoalType($types) {
         array_push(
             $types, 
             [ 
-                'name' => 'html-form', 
-                'label' => 'HTML Forms',
+                'name' => 'wpcf7_contact_form', 
+                'label' => 'Contact Form 7',
                 'itemName' => 'On submit of form',
                 'help' => 'If the visitor submits this form it will add a point to the tested variant.'
             ]
@@ -30,8 +30,8 @@ class HTMLForms extends Integration {
         return $types;
     }
 
-    public function catchFormSubmits($submission) {
-        $formId = $submission->form_id;
+    public function catchFormSubmits($form) {
+        $formId = $form->id();
         $abTestTracking = new ABTestTracking();
 
         $abTestTracking->trackPostId($formId);
