e157499dc8f1dfe3bb8311e36ca1c7ce137fc1dd
Gaya Kessler <gaya@theclevernode.com>
2019-04-05 14:19:07 +0200

    refactor significance to own component

--- src/js/components/Admin/pages/Overview/Overview.js
+++ src/js/components/Admin/pages/Overview/Overview.js
@@ -1,6 +1,7 @@
 // @flow
 
 import React, { Fragment } from 'react';
+import classNames from 'classnames';
 
 import Table from '../../components/Table/Table';
 
@@ -36,6 +37,10 @@ type OverviewData = {
   }[];
 };
 
+function postLink(name: string, link?: string) {
+  return link ? (<a href={link}>{name}</a>) : name;
+}
+
 function Overview({ data }: { data: OverviewData }) {
   const { activeTests } = data;
 
@@ -49,11 +54,79 @@ function Overview({ data }: { data: OverviewData }) {
             <tr>
               <th className="check-column" />
               <th className="column-primary">{__('Started At')}</th>
-              <th>{__('Page')}</th>
+              <th>{__('On Page')}</th>
               <th>{__('Goal')}</th>
               <th className="num">{__('Participants')}</th>
             </tr>
           </thead>
+          <tbody>
+            {activeTests.map(test => (
+              <Fragment>
+                <tr>
+                  <td className="check-column check-column-normal">
+                    <div
+                      className={
+                        classNames(
+                          'indicator',
+                          {
+                            'indicator--on': test.isEnabled,
+                            'indicator--off': !test.isEnabled,
+                          },
+                        )}
+                    />
+                  </td>
+                  <td>{test.startedAt}</td>
+                  <td className="column-primary">{postLink(test.postName, test.postLink)}</td>
+                  <td className="column-primary">{postLink(test.goalName, test.goalLink)}</td>
+                  <td className="num">{test.totalParticipants}</td>
+                </tr>
+                <tr />
+                <tr style={{ display: 'table-row' }} id={`ABTestResults-${test.id}`}>
+                  <td colSpan="5" style={{ display: 'table-cell' }}>
+                    {test.totalParticipants > 0 ? (
+                      <Table className="variations">
+                        <thead>
+                          <tr>
+                            <th className="check-column" />
+                            <th className="column-primary">{__('Conversion Rate')}</th>
+                            <th>{__('Conversions')}</th>
+                            <th>{__('Participants')}</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {test.variants.map(variant => (
+                            <tr className={variant.leading && variant.participants > 0 ? 'ABTestWinning' : 'ABTestLosing'}>
+                              <td className="check-column check-column-normal">
+                                {variant.name}
+                                {variant.id === test.control && ' *'}
+                              </td>
+                              <td className="column-primary">
+                                {`${variant.rate}%`}
+                                {variant.uplift !== 0 && (
+                                  <span className="ABTestUplift">
+                                    {[
+                                      ' (',
+                                      variant.uplift > 0 ? '+' : '',
+                                      variant.uplift,
+                                      '%)',
+                                    ].join('')}
+                                  </span>
+                                )}
+                              </td>
+                              <td>{variant.conversions}</td>
+                              <td>{variant.participants}</td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </Table>
+                    ) : (
+                      <p><em>{__('No results for this test yet.')}</em></p>
+                    )}
+                  </td>
+                </tr>
+              </Fragment>
+            ))}
+          </tbody>
         </Table>
       ) : (
         <Fragment>
