988223b4ea4289017708ae7b952be259d5e28c9a
Gaya Kessler <gaya@theclevernode.com>
2019-04-09 14:30:19 +0200

    add new field to Gutenberg block

--- src/js/components/GeneralSettings/GeneralSettings.js
+++ src/js/components/GeneralSettings/GeneralSettings.js
@@ -3,18 +3,30 @@
 import { i18n, components } from '../../wp';
 
 const { __ } = i18n;
-const { PanelBody, ToggleControl } = components;
+const { PanelBody, ToggleControl, TextControl } = components;
 
 type GeneralSettingsProps = {
+  name: string;
   isEnabled: boolean;
+  onChangeName: (name: string) => void;
   onChangeEnabled: (enabled: boolean) => void;
 };
 
-function GeneralSettings({ isEnabled, onChangeEnabled }: GeneralSettingsProps) {
+function GeneralSettings({
+  name,
+  isEnabled,
+  onChangeName,
+  onChangeEnabled,
+}: GeneralSettingsProps) {
   return (
     <PanelBody title={__('General Settings')}>
+      <TextControl
+        label={__('Name of test')}
+        value={name}
+        onChange={onChangeName}
+      />
       <ToggleControl
-        label="Run this test"
+        label={__('Run this test')}
         help={__(isEnabled ? 'Test is in progress' : 'Showing control variant to every visitor')}
         checked={isEnabled}
         onChange={onChangeEnabled}
