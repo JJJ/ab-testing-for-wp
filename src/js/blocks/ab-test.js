841621728123069958347ed52449b16ee7bbbc33
Gaya Kessler <gaya@theclevernode.com>
2019-04-09 19:21:09 +0200

    select block on onboarding and page navigation

--- src/js/blocks/ab-test.js
+++ src/js/blocks/ab-test.js
@@ -1,6 +1,8 @@
 // @flow @jsx wp.element.createElement
 
+import { Component } from 'react';
 import shortid from 'shortid';
+import queryString from 'query-string';
 
 import {
   i18n,
@@ -30,88 +32,111 @@ const { withDispatch, select } = data;
 type ABTestBlockProps = {
   attributes: ABTestAttributes;
   onDeclareWinner: (id: string) => void;
+  selectBlock: () => void;
 } & GutenbergProps;
 
 const ALLOWED_BLOCKS = ['ab-testing-for-wp/ab-test-block-variant'];
 
 const makeTemplate = variant => ['ab-testing-for-wp/ab-test-block-variant', variant];
 
-function ABTestBlock(props: ABTestBlockProps) {
-  const {
-    clientId,
-    attributes,
-    setAttributes,
-    onDeclareWinner,
-  } = props;
+class ABTestBlock extends Component<ABTestBlockProps> {
+  componentDidMount() {
+    const { attributes, clientId, selectBlock } = this.props;
+    const { test } = queryString.parse(window.location.search);
 
-  const {
-    id,
-    variants,
-    title,
-    postGoal,
-    postGoalType,
-    control,
-    isEnabled,
-    startedAt,
-    completedOnboarding,
-  } = attributes;
+    // check if test from querystring is this test
+    if (attributes.id !== test) return;
 
-  const cancelOnboarding = () => {
-    setOption('completedOnboarding', true);
-    setAttributes({ completedOnboarding: true });
-  };
+    setTimeout(() => {
+      // find the block in the content
+      const block = document.getElementById(`block-${clientId}`);
+      if (!block) return;
 
-  // initialize attributes
-  if (!id) {
-    const { getCurrentPost } = select('core/editor');
+      // scroll to block
+      block.scrollIntoView({ block: 'center' });
 
-    const defaultVariants: ABTestVariant[] = [
-      {
-        id: shortid.generate(),
-        name: 'A',
-        selected: true,
-        distribution: 50,
-      },
-      {
-        id: shortid.generate(),
-        name: 'B',
-        selected: false,
-        distribution: 50,
-      },
-    ];
-
-    setAttributes({
-      id: shortid.generate(),
-      title: sprintf(__('New test on "%s"'), getCurrentPost().title),
-      variants: defaultVariants,
-      postGoal: 0,
-      postGoalType: '',
-      control: defaultVariants[0].id,
-      isEnabled: false,
-    });
+      // select block in editor
+      selectBlock();
+    }, 1000);
   }
 
-  const onSelectVariant = (variantId: string) => {
-    setAttributes({
-      variants: variants.map(variant => ({ ...variant, selected: variant.id === variantId })),
+  render() {
+    const {
+      clientId,
+      attributes,
+      setAttributes,
+      onDeclareWinner,
+      selectBlock,
+    } = this.props;
+
+    const {
+      id,
+      variants,
+      title,
+      postGoal,
+      postGoalType,
+      control,
+      isEnabled,
+      startedAt,
+      completedOnboarding,
+    } = attributes;
+
+    const cancelOnboarding = () => {
+      setOption('completedOnboarding', true);
+      setAttributes({ completedOnboarding: true });
+    };
+
+    // initialize attributes
+    if (!id) {
+      const { getCurrentPost } = select('core/editor');
+
+      const defaultVariants: ABTestVariant[] = [
+        {
+          id: shortid.generate(),
+          name: 'A',
+          selected: true,
+          distribution: 50,
+        },
+        {
+          id: shortid.generate(),
+          name: 'B',
+          selected: false,
+          distribution: 50,
+        },
+      ];
+
+      setAttributes({
+        id: shortid.generate(),
+        title: sprintf(__('New test on "%s"'), getCurrentPost().title),
+        variants: defaultVariants,
+        postGoal: 0,
+        postGoalType: '',
+        control: defaultVariants[0].id,
+        isEnabled: false,
+      });
+    }
+
+    const onSelectVariant = (variantId: string) => {
+      setAttributes({
+        variants: variants.map(variant => ({ ...variant, selected: variant.id === variantId })),
+      });
+    };
+    const onUpdateVariants = (newVariants: ABTestVariant[]) => setAttributes({
+      variants: newVariants,
     });
-  };
-  const onUpdateVariants = (newVariants: ABTestVariant[]) => setAttributes({
-    variants: newVariants,
-  });
-  const onTitleChange = (newTitle: string) => setAttributes({ title: newTitle });
-  const onPostGoalChange = (postId: number) => setAttributes({ postGoal: postId });
-  const onPostGoalTypeChange = (type: string) => setAttributes({ postGoalType: type });
-  const onControlChange = (variantId: string) => setAttributes({ control: variantId });
-  const onEnabledChange = (enabled: boolean) => setAttributes({
-    isEnabled: enabled,
-    // set start time if no start time is known
-    startedAt: enabled && !startedAt ? new Date() : startedAt,
-  });
+    const onTitleChange = (newTitle: string) => setAttributes({ title: newTitle });
+    const onPostGoalChange = (postId: number) => setAttributes({ postGoal: postId });
+    const onPostGoalTypeChange = (type: string) => setAttributes({ postGoalType: type });
+    const onControlChange = (variantId: string) => setAttributes({ control: variantId });
+    const onEnabledChange = (enabled: boolean) => setAttributes({
+      isEnabled: enabled,
+      // set start time if no start time is known
+      startedAt: enabled && !startedAt ? new Date() : startedAt,
+    });
 
-  const selectedVariant = variants.find(test => !!test.selected);
+    const selectedVariant = variants.find(test => !!test.selected);
 
-  const css = `
+    const css = `
       .ABTest--${id} .ABTestVariant { 
         display: none;
       }
@@ -121,59 +146,61 @@ function ABTestBlock(props: ABTestBlockProps) {
       }
     `;
 
-  const showOnboarding = !completedOnboarding && window.innerWidth > 780;
+    const showOnboarding = !completedOnboarding && window.innerWidth > 780;
 
-  return (
-    <div className={`ABTest--${id}`}>
-      {showOnboarding && (
-        <Onboarding
-          cancelOnboarding={cancelOnboarding}
-          clientId={clientId}
+    return (
+      <div className={`ABTest--${id}`}>
+        {showOnboarding && (
+          <Onboarding
+            clientId={clientId}
+            cancelOnboarding={cancelOnboarding}
+            selectBlock={selectBlock}
+          />
+        )}
+        <style>{css}</style>
+        <InspectorControls>
+          <GeneralSettings
+            title={title}
+            isEnabled={isEnabled}
+            onChangeTitle={onTitleChange}
+            onChangeEnabled={onEnabledChange}
+          />
+          <TestResults
+            isEnabled={isEnabled}
+            testId={id}
+            control={control}
+            startedAt={startedAt}
+            onDeclareWinner={onDeclareWinner}
+          />
+          <DistributionSettings
+            variants={variants}
+            onUpdateVariants={onUpdateVariants}
+          />
+          <GoalSelector
+            value={postGoal}
+            type={postGoalType}
+            onChange={onPostGoalChange}
+            onTypeChange={onPostGoalTypeChange}
+          />
+          <ControlSettings
+            value={control}
+            variants={variants}
+            onChange={onControlChange}
+          />
+        </InspectorControls>
+        <InnerBlocks
+          templateLock="all"
+          template={variants.map(makeTemplate)}
+          allowedBlocks={ALLOWED_BLOCKS}
         />
-      )}
-      <style>{css}</style>
-      <InspectorControls>
-        <GeneralSettings
-          title={title}
-          isEnabled={isEnabled}
-          onChangeTitle={onTitleChange}
-          onChangeEnabled={onEnabledChange}
-        />
-        <TestResults
-          isEnabled={isEnabled}
-          testId={id}
-          control={control}
-          startedAt={startedAt}
-          onDeclareWinner={onDeclareWinner}
-        />
-        <DistributionSettings
+        <VariantSelector
           variants={variants}
-          onUpdateVariants={onUpdateVariants}
+          onSelectVariant={onSelectVariant}
         />
-        <GoalSelector
-          value={postGoal}
-          type={postGoalType}
-          onChange={onPostGoalChange}
-          onTypeChange={onPostGoalTypeChange}
-        />
-        <ControlSettings
-          value={control}
-          variants={variants}
-          onChange={onControlChange}
-        />
-      </InspectorControls>
-      <InnerBlocks
-        templateLock="all"
-        template={variants.map(makeTemplate)}
-        allowedBlocks={ALLOWED_BLOCKS}
-      />
-      <VariantSelector
-        variants={variants}
-        onSelectVariant={onSelectVariant}
-      />
-      <BoxShadow />
-    </div>
-  );
+        <BoxShadow />
+      </div>
+    );
+  }
 }
 
 const edit = withDispatch((dispatch, props) => {
@@ -201,6 +228,12 @@ const edit = withDispatch((dispatch, props) => {
 
       // remove test
       removeBlock(clientId);
+    },
+    selectBlock() {
+      const { selectBlock } = dispatch('core/editor');
+      const { openGeneralSidebar } = dispatch('core/edit-post');
+      selectBlock(clientId);
+      openGeneralSidebar('edit-post/block');
     },
   };
 })(ABTestBlock);
