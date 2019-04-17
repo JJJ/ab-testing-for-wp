3e1e50321cdb5b5f3392a12bc0305c3a7d860275
Gaya Kessler <gaya@theclevernode.com>
2019-04-17 07:36:47 +0200

    convert to react

--- flow-typed/ab-test-for-wp.js
+++ flow-typed/ab-test-for-wp.js
@@ -33,3 +33,33 @@ declare type ABTestResult = {
   participants: number;
   conversions: number;
 };
+
+declare type TestVariant = {
+  id: string;
+  conversions: number;
+  participants: number;
+  leading: boolean;
+  name: string;
+  rate: number;
+  uplift: number;
+}
+
+declare type TestData = {
+  id: string;
+  control: string;
+  title: string;
+  goalName: string;
+  goalType: string;
+  goalLink?: string;
+  postId: string;
+  postGoal: string;
+  postType: string;
+  postName: string;
+  postLink?: string;
+  postDeleteLink?: string;
+  startedAt: number;
+  totalParticipants: number;
+  isArchived: string;
+  isEnabled: boolean;
+  variants: TestVariant[];
+};
--- src/js/components/Admin/pages/Overview/Overview.js
+++ src/js/components/Admin/pages/Overview/Overview.js
@@ -15,36 +15,6 @@ import './Overview.css';
 
 const { __ } = i18n;
 
-type TestVariant = {
-  id: string;
-  conversions: number;
-  participants: number;
-  leading: boolean;
-  name: string;
-  rate: number;
-  uplift: number;
-}
-
-type TestData = {
-  id: string;
-  control: string;
-  title: string;
-  goalName: string;
-  goalType: string;
-  goalLink?: string;
-  postId: string;
-  postGoal: string;
-  postType: string;
-  postName: string;
-  postLink?: string;
-  postDeleteLink?: string;
-  startedAt: number;
-  totalParticipants: number;
-  isArchived: string;
-  isEnabled: boolean;
-  variants: TestVariant[];
-};
-
 type OverviewData = {
   activeTests: TestData[];
 };
