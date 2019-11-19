0ef1eee82faee8f51781b6aea6423af7cfd4d42c
Gaya Kessler <gaya@theclevernode.com>
2019-11-19 20:39:36 +0100

    fix version bump script

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -4,9 +4,9 @@ read -p "Enter version new: " VERSION
 echo "Version is" $VERSION
 
 # Add release numbers
-sed -i .copy -e "s/Stable tag: [[:digit:]]\.[[:digit:]]\.[[:digit:]]/Stable tag: $VERSION/" ./README.txt
-sed -i .copy -e "s/\"version\": \"[[:digit:]]\.[[:digit:]]\.[[:digit:]]\"/\"version\": \"$VERSION\"/" ./package.json
-sed -i .copy -e "s/@version [[:digit:]]\.[[:digit:]]\.[[:digit:]]/@version $VERSION/" ./ab-testing-for-wp.php
-sed -i .copy -e "s/Version: [[:digit:]]\.[[:digit:]]\.[[:digit:]]/Version: $VERSION/" ./ab-testing-for-wp.php
+sed -i .copy -e "s/Stable tag: [0-9]*\.[0-9]*\.[0-9]*/Stable tag: $VERSION/" ./README.txt
+sed -i .copy -e "s/\"version\": \"[0-9]*\.[0-9]*\.[0-9]*\"/\"version\": \"$VERSION\"/" ./package.json
+sed -i .copy -e "s/@version [0-9]*\.[0-9]*\.[0-9]*/@version $VERSION/" ./ab-testing-for-wp.php
+sed -i .copy -e "s/Version: [0-9]*\.[0-9]*\.[0-9]*/Version: $VERSION/" ./ab-testing-for-wp.php
 
 rm *.copy
