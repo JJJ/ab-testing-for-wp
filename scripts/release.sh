9020ade5a441371eced49f69a7c20f638494d504
Gaya Kessler <gaya@theclevernode.com>
2019-11-21 19:44:37 +0100

    remove svn dir from release step

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-read -p "Enter SVN dir [./svn]: " SVN_DIR
-SVN_DIR=${SVN_DIR:-./svn}
-echo "SVN directory is" $SVN_DIR
-
 VERSION=$(cat ab-testing-for-wp.php | grep "Version: \(.*\)" | tr ' ' '\n' | tail -1)
 
 echo "Release version $VERSION (y/n)?"
