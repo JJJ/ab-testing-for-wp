aee76a9e74802a3b6adffd0cef39b49c4dd71106
Gaya Kessler <gaya@theclevernode.com>
2019-12-02 16:16:24 +0100

    convert VariantSelector to ts

--- src/js/components/VariantSelector/VariantSelector.tsx
+++ src/js/components/VariantSelector/VariantSelector.tsx
@@ -1,37 +1,30 @@
 // @flow
 
 import React from 'react';
-
-import { components } from '../../wp';
+import { ButtonGroup, IconButton } from '@wordpress/components';
 
 import './VariantSelector.css';
-
-const {
-  ButtonGroup,
-  IconButton,
-} = components;
 
 type VariantSelectorProps = {
   variants: ABTestVariant[];
   onSelectVariant: (id: string) => void;
 }
 
-function VariantSelector({ variants, onSelectVariant }: VariantSelectorProps) {
-  return (
-    <div className="ab-test-for-wp__VariantSelector">
-      <ButtonGroup>
-        {variants.map(variant => (
-          <IconButton
-            isToggled={variant.selected}
-            onClick={() => onSelectVariant(variant.id)}
-          >
-            {variant.name}
-          </IconButton>
-        ))}
-        <IconButton icon="admin-generic" />
-      </ButtonGroup>
-    </div>
-  );
-}
+const VariantSelector: React.FC<VariantSelectorProps> = ({ variants, onSelectVariant }) => (
+  <div className="ab-test-for-wp__VariantSelector">
+    <ButtonGroup>
+      {variants.map((variant) => (
+        <IconButton
+          icon={<div />}
+          isToggled={variant.selected}
+          onClick={(): void => onSelectVariant(variant.id)}
+        >
+          {variant.name}
+        </IconButton>
+      ))}
+      <IconButton icon="admin-generic" />
+    </ButtonGroup>
+  </div>
+);
 
 export default VariantSelector;
