3fcd6e847a74d5025f2ece25d40317b749270efe
Gaya Kessler <gaya@theclevernode.com>
2020-01-07 16:18:18 +0100

    basic cypress setup

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,17 +1,5 @@
-function setupDatabase() {
-  const user = Cypress.env('MYSQL_USER');
-  const password = Cypress.env('MYSQL_PASSWORD');
-  const database = Cypress.env('MYSQL_DATABASE');
-  const port = Cypress.env('MYSQL_PORT');
-  const host = Cypress.env('MYSQL_HOST');
-
-  const mysqlBase = `mysql -u"${user}" --password="${password}" -h"${host}" -P"${port}"`;
-
-  // cy.exec(`${mysqlBase} -e "DROP DATABASE IF EXISTS \`${database}\`;"`);
-}
-
-describe('The Home Page', function() {
-  it('successfully loads', function() {
-    cy.visit('/') // change URL to match your dev URL
-  })
-})
+describe('The Home Page', () => {
+  it('successfully loads', () => {
+    cy.visit('/');
+  });
+});
