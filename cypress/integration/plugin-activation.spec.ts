4fba15764fb2c21eec3679711f63aa1e64431bfe
Gaya Kessler <gaya@theclevernode.com>
2020-01-13 11:42:38 +0100

    use is-active for checking plugin

--- cypress/integration/plugin-activation.spec.ts
+++ cypress/integration/plugin-activation.spec.ts
@@ -1,9 +1,7 @@
 describe('Plugin activation', () => {
   it('Successfully loads with plugin installed', () => {
     // check if activated successfully
-    cy.exec('npm run e2e:wp-cli -- plugin list')
-      .its('stdout')
-      .should('contain', 'ab-testing-for-wp');
+    cy.exec('npm run e2e:wp-cli -- plugin is-active ab-testing-for-wp');
 
     // see if front page still works
     cy.visit('/');
