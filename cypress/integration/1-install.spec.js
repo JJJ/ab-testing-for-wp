4ee675d631b6a9f2c9685555c05240cb332e24c8
Gaya Kessler <gaya@theclevernode.com>
2020-01-13 15:54:13 +0100

    fix password input

--- cypress/integration/1-install.spec.js
+++ cypress/integration/1-install.spec.js
@@ -4,13 +4,12 @@ describe('WordPress installation', () => {
     cy.gotoAdmin('install.php');
 
     // select language
-    const button = cy.get('#language-continue');
-    if (button) button.click();
+    cy.get('input[type=submit]').click();
 
     // fill out form
     cy.get('#weblog_title').type('A/B Testing for WordPress E2E tests');
     cy.get('#user_login').type(Cypress.env('WP_USER'));
-    cy.get('#pass1-text').clear().type(Cypress.env('WP_PASSWORD'));
+    cy.get('#pass1').clear({ force: true }).type(Cypress.env('WP_PASSWORD'), { force: true });
     cy.get('#admin_email').type('e2e@abtestingforwp.com');
     cy.get('#submit').click();
   });
