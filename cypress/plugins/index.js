504e8391518917315e43f255f5b04bb7783c41a8
Gaya Kessler <gaya@theclevernode.com>
2020-01-15 16:27:58 +0100

    remove window fallback

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -5,10 +5,4 @@ module.exports = (on) => {
   on('file:preprocessor', webpack({
     webpackOptions: require('../../webpack.config'),
   }));
-
-  on('task', {
-    'npm-run': (task) => {
-      return shell(`npm run ${task}`);
-    }
-  })
 };
