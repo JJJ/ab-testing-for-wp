fb7d93deb36d881eccb1d1212830039f5ee4786e
Gaya Kessler <gaya@theclevernode.com>
2020-01-16 16:24:48 +0100

    add placeholder test description

---
+++ cypress/integration/stand-alone.spec.ts
@@ -0,0 +1,9 @@
+describe('Stand alone A/B tests', () => {
+  it('Should allow the user to create a stand alone test', () => {
+    expect(1).to.equal(1);
+  });
+
+  it('Enable to pick stand alone tests when adding A/B test in content', () => {
+    expect(1).to.equal(1);
+  });
+});
