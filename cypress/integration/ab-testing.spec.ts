4a45c9f4568529b3631d35b9d3bb9a2759da1553
Gaya Kessler <gaya@theclevernode.com>
2020-01-16 13:41:20 +0100

    add test for testing if test can start

--- cypress/integration/ab-testing.spec.ts
+++ cypress/integration/ab-testing.spec.ts
@@ -19,11 +19,15 @@ describe('A/B Testing', () => {
     cy.addTestInEditor();
 
     // test if succeeded
-    cy.contains('Button for Test Variant "A"').should('be.visible');
+    cy.contains('Button for Test Variant "A"')
+      .should('be.visible');
 
-    cy.get('.components-button-group > button').eq(1).click();
+    cy.get('.components-button-group > button')
+      .eq(1)
+      .click();
 
-    cy.contains('Button for Test Variant "B"').should('be.visible');
+    cy.contains('Button for Test Variant "B"')
+      .should('be.visible');
   });
 
   it('Can add switch between variants', () => {
@@ -33,8 +37,11 @@ describe('A/B Testing', () => {
     cy.addTestInEditor();
 
     // test if succeeded
-    cy.get('.components-button-group > button').eq(1).click();
-    cy.contains('Button for Test Variant "B"').should('be.visible');
+    cy.get('.components-button-group > button')
+      .eq(1)
+      .click();
+    cy.contains('Button for Test Variant "B"')
+      .should('be.visible');
   });
 
   it('Can save test and displays control on frontend', () => {
@@ -45,14 +52,18 @@ describe('A/B Testing', () => {
     cy.addTestInEditor();
 
     // save post
-    cy.get('.editor-post-publish-panel__toggle').click();
-    cy.get('.editor-post-publish-panel__header-publish-button > .components-button').click();
+    cy.get('.editor-post-publish-panel__toggle')
+      .click();
+    cy.get('.editor-post-publish-panel__header-publish-button > .components-button')
+      .click();
 
     // go to post
-    cy.get('.post-publish-panel__postpublish-buttons > a.components-button').click();
+    cy.get('.post-publish-panel__postpublish-buttons > a.components-button')
+      .click();
 
     // check if it renders button
-    cy.get('.wp-block-button__link').contains('Button for Test Variant “A”');
+    cy.get('.wp-block-button__link')
+      .contains('Button for Test Variant “A”');
   });
 
   it('Can save test and displays variant "B" as control on frontend', () => {
@@ -62,7 +73,8 @@ describe('A/B Testing', () => {
     cy.addTestInEditor();
 
     // open test options
-    cy.get('.components-button-group > :nth-child(3)').click();
+    cy.get('.components-button-group > :nth-child(3)')
+      .click();
 
     // change variant to B
     cy.get('#inspector-select-control-1')
@@ -70,14 +82,18 @@ describe('A/B Testing', () => {
       .select('B');
 
     // save post
-    cy.get('.editor-post-publish-panel__toggle').click();
-    cy.get('.editor-post-publish-panel__header-publish-button > .components-button').click();
+    cy.get('.editor-post-publish-panel__toggle')
+      .click();
+    cy.get('.editor-post-publish-panel__header-publish-button > .components-button')
+      .click();
 
     // go to post
-    cy.get('.post-publish-panel__postpublish-buttons > a.components-button').click();
+    cy.get('.post-publish-panel__postpublish-buttons > a.components-button')
+      .click();
 
     // check if it renders button
-    cy.get('.wp-block-button__link').contains('Button for Test Variant “B”');
+    cy.get('.wp-block-button__link')
+      .contains('Button for Test Variant “B”');
   });
 
   it('Can change distribution of variants', () => {
@@ -87,7 +103,8 @@ describe('A/B Testing', () => {
     cy.addTestInEditor();
 
     // open test options
-    cy.get('.components-button-group > :nth-child(3)').click();
+    cy.get('.components-button-group > :nth-child(3)')
+      .click();
 
     // change range input
     cy.changeRange('#inspector-range-control-2', 75);
@@ -99,16 +116,20 @@ describe('A/B Testing', () => {
       .should('have.value', '25');
 
     // save post
-    cy.get('.editor-post-publish-panel__toggle').click();
-    cy.get('.editor-post-publish-panel__header-publish-button > .components-button').click();
+    cy.get('.editor-post-publish-panel__toggle')
+      .click();
+    cy.get('.editor-post-publish-panel__header-publish-button > .components-button')
+      .click();
 
     // reload and skip onboarding
-    cy.location().then(({ pathname, search }) => {
-      cy.visit(`${pathname}${search}&skipOnboarding=1`);
-    });
+    cy.location()
+      .then(({ pathname, search }) => {
+        cy.visit(`${pathname}${search}&skipOnboarding=1`);
+      });
 
     // open test options
-    cy.get('.components-button-group > :nth-child(3)').click();
+    cy.get('.components-button-group > :nth-child(3)')
+      .click();
 
     // check if values saved correctly
     cy.get(':nth-child(2) > .components-base-control__field > .components-range-control__number')
@@ -124,7 +145,8 @@ describe('A/B Testing', () => {
     cy.addTestInEditor();
 
     // open test options
-    cy.get('.components-button-group > :nth-child(3)').click();
+    cy.get('.components-button-group > :nth-child(3)')
+      .click();
 
     // change goal type
     cy.get('#inspector-select-control-2')
@@ -137,21 +159,63 @@ describe('A/B Testing', () => {
       .select('Sample Page');
 
     // save post
-    cy.get('.editor-post-publish-panel__toggle').click();
-    cy.get('.editor-post-publish-panel__header-publish-button > .components-button').click();
+    cy.get('.editor-post-publish-panel__toggle')
+      .click();
+    cy.get('.editor-post-publish-panel__header-publish-button > .components-button')
+      .click();
 
     // reload and skip onboarding
-    cy.location().then(({ pathname, search }) => {
-      cy.visit(`${pathname}${search}&skipOnboarding=1`);
-    });
+    cy.location()
+      .then(({ pathname, search }) => {
+        cy.visit(`${pathname}${search}&skipOnboarding=1`);
+      });
 
     // open test options
-    cy.get('.components-button-group > :nth-child(3)').click();
+    cy.get('.components-button-group > :nth-child(3)')
+      .click();
 
     // check saved values
     cy.get('#inspector-select-control-1')
       .scrollIntoView()
       .should('contain', 'Pages');
-    cy.get('#inspector-select-control-2').should('contain', 'Sample Page');
+    cy.get('#inspector-select-control-2')
+      .should('contain', 'Sample Page');
+  });
+
+  it('Can start a test for tracking', () => {
+    cy.visitAdmin('post-new.php?skipOnboarding=1');
+
+    // add default test
+    cy.addTestInEditor();
+
+    // open test options
+    cy.get('.components-button-group > :nth-child(3)')
+      .click();
+
+    // start test
+    cy.get('#inspector-toggle-control-1')
+      .click();
+
+    // gets declare winner tab
+    cy.contains('Results so far');
+
+    // save post
+    cy.get('.editor-post-publish-panel__toggle')
+      .click();
+    cy.get('.editor-post-publish-panel__header-publish-button > .components-button')
+      .click();
+
+    // reload and skip onboarding
+    cy.location()
+      .then(({ pathname, search }) => {
+        cy.visit(`${pathname}${search}&skipOnboarding=1`);
+      });
+
+    // open test options
+    cy.get('.components-button-group > :nth-child(3)')
+      .click();
+
+    // gets declare winner tab
+    cy.contains('Results so far');
   });
 });
