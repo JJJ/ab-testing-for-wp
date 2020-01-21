8f253d5279a17cd6bf545026d2128a8bd8313ce9
Gaya Kessler <gaya@theclevernode.com>
2020-01-21 16:54:00 +0100

    catch links and submits for tracking

--- src/js/frontend/handleTestTracking.ts
+++ src/js/frontend/handleTestTracking.ts
@@ -1,6 +1,33 @@
 import apiFetch from '@wordpress/api-fetch';
 
+function handleTracking(
+  getUrl: (target: EventTarget | null) => string | undefined,
+): (e: Event) => void {
+  return function trackingEventHandler(e): void {
+    if (!navigator.sendBeacon) return;
+
+    const url = getUrl(e.target);
+
+    if (!url) return;
+
+    navigator.sendBeacon('/ab-testing-for-wp/v1/outbound', JSON.stringify({ url }));
+  };
+}
+
 function handleTestTracking(): void {
+  // listen to anchor navigation
+  window.addEventListener('click', handleTracking((target): string | undefined => {
+    if (!target || !(target as HTMLAnchorElement).href) return undefined;
+    return (target as HTMLAnchorElement).href;
+  }));
+
+  // listen to form submits
+  window.addEventListener('submit', handleTracking((target): string | undefined => {
+    if (!target || !(target as HTMLFormElement).action) return undefined;
+    return (target as HTMLFormElement).action;
+  }));
+
+  // track every page
   if (!ABTestingForWP || !ABTestingForWP.postId) return;
   apiFetch({ path: `/ab-testing-for-wp/v1/track?post=${ABTestingForWP.postId}` });
 }
