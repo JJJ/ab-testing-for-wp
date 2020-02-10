83b896ae02f3aba0fd58a988bd4f86473161b571
Gaya Kessler <gaya@theclevernode.com>
2020-02-10 14:24:05 +0100

    extract logic from distribution component

--- src/js/components/VariantSettings/DistributionSettings.tsx
+++ src/js/components/VariantSettings/DistributionSettings.tsx
@@ -3,51 +3,21 @@ import { __, sprintf } from '@wordpress/i18n';
 import { RangeControl } from '@wordpress/components';
 
 type DistributionSettingsProps = {
-  variants: ABTestVariant[];
-  onUpdateVariants: (variants: ABTestVariant[]) => void;
+  variant: ABTestVariant;
+  onUpdateDistribution: (id: string, newDistribution: number | undefined) => void;
 };
 
 const DistributionSettings: React.FC<DistributionSettingsProps> = ({
-  variants,
-  onUpdateVariants,
-}) => {
-  const onUpdateDistribution = (id: string, newDistribution: number | undefined): void => {
-    if (!newDistribution) return;
-
-    const otherVariants = variants.filter((test) => test.id !== id);
-    const combinedLeft = otherVariants.reduce((a, b) => a + (b.distribution || 0), 0);
-    const rate = combinedLeft !== 0 ? (100 - newDistribution) / combinedLeft : 0;
-
-    onUpdateVariants(variants.map((variant) => {
-      if (variant.id === id) {
-        return {
-          ...variant,
-          distribution: newDistribution,
-        };
-      }
-
-      return {
-        ...variant,
-        distribution: Math.round(combinedLeft === 0
-          ? (100 - newDistribution) / otherVariants.length
-          : variant.distribution * rate),
-      };
-    }));
-  };
-
-  return (
-    <>
-      {variants.map((variant) => (
-        <RangeControl
-          label={sprintf(__('Variant %s distribution', 'ab-testing-for-wp'), variant.name)}
-          value={variant.distribution}
-          onChange={(nextDistribution): void => onUpdateDistribution(variant.id, nextDistribution)}
-          min={0}
-          max={100}
-        />
-      ))}
-    </>
-  );
-};
+  variant,
+  onUpdateDistribution,
+}) => (
+  <RangeControl
+    label={sprintf(__('Variant %s distribution', 'ab-testing-for-wp'), variant.name)}
+    value={variant.distribution}
+    onChange={(nextDistribution): void => onUpdateDistribution(variant.id, nextDistribution)}
+    min={0}
+    max={100}
+  />
+);
 
 export default DistributionSettings;
--- src/js/components/VariantSettings/VariantSettings.tsx
+++ src/js/components/VariantSettings/VariantSettings.tsx
@@ -14,18 +14,46 @@ type VariantSettingsProps = {
 
 const VariantSettings: React.FC<VariantSettingsProps> = ({
   control, variants, onControlChange, onUpdateVariants,
-}) => (
-  <PanelBody title={__('Variantions', 'ab-testing-for-wp')}>
-    <ControlSettings
-      value={control}
-      variants={variants}
-      onChange={onControlChange}
-    />
-    <DistributionSettings
-      variants={variants}
-      onUpdateVariants={onUpdateVariants}
-    />
-  </PanelBody>
-);
+}) => {
+  const onUpdateDistribution = (id: string, newDistribution: number | undefined): void => {
+    if (!newDistribution) return;
+
+    const otherVariants = variants.filter((test) => test.id !== id);
+    const combinedLeft = otherVariants.reduce((a, b) => a + (b.distribution || 0), 0);
+    const rate = combinedLeft !== 0 ? (100 - newDistribution) / combinedLeft : 0;
+
+    onUpdateVariants(variants.map((variant) => {
+      if (variant.id === id) {
+        return {
+          ...variant,
+          distribution: newDistribution,
+        };
+      }
+
+      return {
+        ...variant,
+        distribution: Math.round(combinedLeft === 0
+          ? (100 - newDistribution) / otherVariants.length
+          : variant.distribution * rate),
+      };
+    }));
+  };
+
+  return (
+    <PanelBody title={__('Variantions', 'ab-testing-for-wp')}>
+      <ControlSettings
+        value={control}
+        variants={variants}
+        onChange={onControlChange}
+      />
+      {variants.map((variant) => (
+        <DistributionSettings
+          variant={variant}
+          onUpdateDistribution={onUpdateDistribution}
+        />
+      ))}
+    </PanelBody>
+  );
+};
 
 export default VariantSettings;
